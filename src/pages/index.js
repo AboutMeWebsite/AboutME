440186aa7372d7b9b8eebe9eae04e5ac01d4e38f
LuminMCB <tmschmitz@cox.net>
2022-11-13 11:16:18 -0700

    added Tylers page

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,6 +31,10 @@ export default function Home() {
             <h3>Edward's Page &rarr;</h3>
             <p>Click here to navigate to Edward's page</p>
           </a>
+          <a href='/tylerSchmitz' className='card'>
+            <h3>Tyler's Page &rarr;</h3>
+            <p>Click here to navigate to Tyler's page</p>
+          </a>
           <a
             href='https://github.com/wangalec/website-template'
             className='card'>
---
+++ src/pages/tylerSchmitz.js
@@ -0,0 +1,120 @@
+export default function examplePage() {
+    return (
+      <div className='container'>
+        <title>Website</title>
+        <link rel='icon' href='/favicon.ico' />
+
+        <main>
+          <div className='underline'>
+            <h1 className='title'>Hi! I'm Tyler.</h1>
+          </div>
+          <img
+            className='pic'
+            src='https://cdn.discordapp.com/attachments/458484587543265300/1041415523012968448/pulse-lumin.png'
+            alt='ron burgundy'
+          />
+
+          <p className='description'>
+            I'm a Californian who moved to Colorado to study CS at CU Boulder. I've always loved coding, and I'm fascinated by the world of
+            visual effects. I hope to someday work in the field of computer graphics, more specifically involved with simulations (fluid, fire, etc). Right now,
+            I work as a freelance video editor and am a part of Team Pulse, the most well known Rocket League freestyling organization.
+          </p>
+
+          <a className='button' href='/'>
+            <p> Back to Home Page</p>
+          </a>
+        </main>
+
+        <style jsx>{`
+          .container {
+            min-height: 100vh;
+            padding: 0 0.5rem;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            max-width: 60rem;
+            margin: auto;
+          }
+
+          main {
+            padding: 5rem 0;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+
+          .title {
+            margin: 0;
+            line-height: 1.15;
+            font-size: 4rem;
+          }
+
+          .underline {
+            /* border-bottom: solid black; */
+            margin-bottom: 1.5rem;
+          }
+
+          .description {
+            line-height: 1.5;
+            font-size: 1.5rem;
+          }
+
+          .pic {
+            height: 450px;
+            width: 450px;
+          }
+
+          .button {
+            /* margin: .5rem; */
+            flex-basis: 45%;
+            padding: 0.5rem;
+            text-align: left;
+            color: inherit;
+            text-decoration: none;
+            border: 1px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+          }
+
+          .button:hover,
+          .button:focus,
+          .button:active {
+            color: #0070f3;
+            border-color: #0070f3;
+          }
+
+          .button p {
+            font-size: 1rem;
+            font-weight: 600;
+          }
+
+          code {
+            background: #fafafa;
+            border-radius: 5px;
+            padding: 0.75rem;
+            font-size: 1.1rem;
+            font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+              DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+          }
+        `}</style>
+
+        <style jsx global>{`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+          }
+
+          * {
+            box-sizing: border-box;
+          }
+        `}</style>
+      </div>
+    );
+}
